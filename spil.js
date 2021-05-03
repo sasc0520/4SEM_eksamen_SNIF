018f9833b07d19de415646023e47a0fba60b4ab3
Sascha <sasc0520@stud.kea.dk>
2021-05-03 17:42:29 +0200

    spil side stylet, ikke helt færdig

--- spil.js
+++ spil.js
@@ -1,5 +1,30 @@
 "use strict";
 window.addEventListener("load", init);
 function init() {
-    console.log("here we go");
+    const games = "spil.json";
+    const array = [];
+    fetchGames(games, array);
+}
+
+async function fetchGames(games, array) {
+    const response = await fetch(games);
+    array = await response.json();
+    displayGames(array);
+}
+
+function displayGames(array) {
+    const contentHolder = document.querySelector("main");
+    const gameTemplate = document.querySelector("template");
+    contentHolder.textContent = "";
+    array.forEach(game => {
+        let clone = gameTemplate.cloneNode(true).content;
+        clone.querySelector(".title").textContent = game.title;
+        clone.querySelector("img").src = game.image;
+
+        clone.querySelector(".game").addEventListener("click", () => {
+            location.href = `single.html?id=${game.id}`;
+          });
+
+        contentHolder.appendChild(clone);
+    });
 }
