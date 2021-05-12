02f601430f46cc9ddc6eed4e714609ded48429b5
Sascha <sasc0520@stud.kea.dk>
2021-05-12 23:29:09 +0200

    animationer ved hover på landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,11 +1,17 @@
 "use strict";
 
 window.addEventListener("load", () => {
-    fetchSVG();
+    document.querySelector(".blue_button").addEventListener("mouseover", () => {
+        document.querySelector(".boern .img_container").classList.add("wiggle");
+        document.querySelector(".blue_button").addEventListener("mouseout", () => {
+            document.querySelector(".boern .img_container").classList.remove("wiggle");
+        });
+    });
+    document.querySelector("#container > div.btn_container > a > button").addEventListener("mouseover", () => {
+        document.querySelector("#container > div.img_container").classList.add("float");
+        document.querySelector("#container > div.btn_container > a > button").addEventListener("mouseout", () => {
+            document.querySelector("#container > div.img_container").classList.remove("float");
+        });
+    });
 });
 
-async function fetchSVG() {
-    const response = await fetch("img/logo.svg");
-    const svgLogo = await response.text();
-    document.querySelector(".svg_container").innerHTML = svgLogo; 
-}
